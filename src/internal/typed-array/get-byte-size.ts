30f04a9a3239dafb17c41f43ed7f1eaab4647cb2
Kris Nye <krisnye@gmail.com>
2025-07-04 15:50:19 -0700

    bumped version

--- src/internal/typed-array/get-byte-size.ts
+++ src/internal/typed-array/get-byte-size.ts
@@ -19,7 +19,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.*/
-import type { TypedArrayConstructor } from "./typed-array-constructer.js";
+import type { TypedArrayConstructor } from "../../types/typed-array-constructer.js";
 
 export const getByteSize = (typedArrayConstructor: TypedArrayConstructor) => {
     if (typedArrayConstructor === Int32Array) {
