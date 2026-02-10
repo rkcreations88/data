19299bf79458032be5398ca9d126092041405408
Kris Nye <krisnye@gmail.com>
2026-02-10 09:13:14 -0800

    remove data * exports and fix data-react bundle

---
+++ packages/data-react/src/index.ts
@@ -0,0 +1,7 @@
+// © 2026 Adobe. MIT License. See /LICENSE for details.
+
+export { DatabaseContext, DatabaseProvider } from "./context/database-context.js";
+export type { DatabaseProviderProps } from "./context/database-context.js";
+export { useDatabase } from "./hooks/use-database.js";
+export { useObservable } from "./hooks/use-observable.js";
+export { useObservableValues } from "./hooks/use-observable-values.js";
